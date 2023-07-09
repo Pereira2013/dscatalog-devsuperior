b6a75d2b098ac588bb945d61467fabc938f383ce
robsonpereira <robson.pereira@sensedia.com>
2023-07-09 09:46:08 -0300

    feat:Post category

--- dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -34,4 +34,11 @@ public class CategoryService {
         Category entity = optinalTheEntity.orElseThrow(() -> new EntityNoutFoundException("Entity not found"));
         return new CategoryDTO(entity);
     }
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
 }
