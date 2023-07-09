80a4f1f16dfa35cd49f4e7701c47e71ba5a5554b
robsonpereira <robson.pereira@sensedia.com>
2023-07-09 09:19:07 -0300

    feat:Find Category by id, exception handling

--- dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNoutFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,5 +27,11 @@ public class CategoryService {
             listDto.add(new CategoryDTO(cat));
         }
         return listDto;*/
+    }
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> optinalTheEntity = repository.findById(id);
+        Category entity = optinalTheEntity.orElseThrow(() -> new EntityNoutFoundException("Entity not found"));
+        return new CategoryDTO(entity);
     }
 }
---
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNoutFoundException.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class EntityNoutFoundException extends RuntimeException {
+    public EntityNoutFoundException(String msg) {
+        super(msg);
+    }
+}
